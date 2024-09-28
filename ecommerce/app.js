c64a69f7368b869fd01878852847198904d68d57
Norman Rivera <normanrivera005@gmail.com>
2024-09-28 13:47:02 +0000

    another one

--- ecommerce/app.js
+++ ecommerce/app.js
@@ -1,12 +1,19 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const routess = require('./routess/router');
+const mysql = require('mysql2');
+const path = require('path');
 const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.urlencoded({extended:true}));
 app.use('/', routess);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/public/images', express.static('public/images'));
+
+
+
 app.listen(4000, ()=>{
     console.log('server running on http://localhost:4000');
 }); 
--- ecommerce/routess/router.js
+++ ecommerce/routess/router.js
@@ -1,6 +1,19 @@
 const express = require('express');
+const multer = require('multer');
+const path = require('path');
 const router = express.Router();
 const main = require('../controller/Main');
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/'); 
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + path.extname(file.originalname)); 
+    }
+});
+
+const upload = multer({ storage: storage });
 
 router.get('/', main.login);
 router.get('/login', main.login);

574a981b51d9ad8660aadda21b0605b5e0f76dec
Benedict R Rudabia <rodabeni27@gmail.com>
2024-10-05 22:53:28 +0800

    user

--- ecommerce/models/userModel.js
+++ ecommerce/models/userModel.js
@@ -10,5 +10,15 @@ const user = {
             });
         });
     },
+
+    updateUser: (id, { fName, lName, email, phone }) => {
+        return new Promise((resolve, reject) => {
+            const query = 'UPDATE users SET fName = ?, lName = ?, email = ?, phone = ? WHERE id = ?';
+            db.execute(query, [fName, lName, email, phone, id], (err, results) => {
+                if (err) return reject(err);
+                resolve(results);
+            });
+        });
+    }
 };
 module.exports = user;
